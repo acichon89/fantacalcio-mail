8a30b36bf9a01c6cd6979efc9352b7e7571b2f57
acichon <acichon89@gmail.com>
2017-07-16 21:56:30 +0200

    fixed column names in entity

--- src/main/java/com/javangarda/fantacalcio/mail/application/internal/storage/SentEmail.java
+++ src/main/java/com/javangarda/fantacalcio/mail/application/internal/storage/SentEmail.java
@@ -5,6 +5,7 @@ import com.javangarda.fantacalcio.commons.entities.DefaultEntity;
 import lombok.Getter;
 import lombok.ToString;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.time.Instant;
@@ -16,9 +17,11 @@ public class SentEmail extends DefaultEntity<String> {
 
     @Getter
     private String email;
+    @Column(name = "sent_date_time")
     @Getter
     private Instant sentDateTime;
     @Getter
+    @Column(name = "html_content")
     private String htmlContent;
     @Getter
     private String title;
